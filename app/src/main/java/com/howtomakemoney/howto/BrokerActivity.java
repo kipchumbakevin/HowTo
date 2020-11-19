df627daf5656974e6be93caed46e417648fcb1c1
kipchumbakevin <kipchumbakevin28@gmail.com>
2020-11-19 22:54:08 +0300

    commit

--- app/src/main/java/com/howtomakemoney/howto/BrokerActivity.java
+++ app/src/main/java/com/howtomakemoney/howto/BrokerActivity.java
@@ -3,24 +3,47 @@ package com.howtomakemoney.howto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class BrokerActivity extends AppCompatActivity {
-    TextView message;
+    TextView message,learnmore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broker);
         message = findViewById(R.id.meso);
+        learnmore = findViewById(R.id.learnmore);
 
         setText(getString(R.string.broker));
+
+        learnmore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.investopedia.com/ask/answers/050715/how-does-insurance-broker-make-money.asp");
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                // To count with Play market backstack, After pressing back button,
+                // to taken back to our application, we need to add following flags to intent.
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.investopedia.com/ask/answers/050715/how-does-insurance-broker-make-money.asp")));
+                }
+            }
+        });
     }
 
     @Override
